e3c315e4b41d97d0c290a5916665d29000ed467b
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-05-24 21:37:22 -0500

    Control Statements and Loops exercise 1 through 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,47 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
 
     public static void main(String[] args) {
+
+        int i = 5;
+
+        while (i < 15){
+            System.out.println(i + "!");
+            i++;
+        }
+
+        System.out.println("BANANA!");
+
+        long j = 2;
+        do {
+
+            System.out.println(j);
+            j *= j;
+
+        } while (j < 1000000); // I can't get the requisite result.  Adding a zero gives infinite zeros as a result.
+
+
+        //Fizzbuzz!//
+
+        int k = 1;
+
+        for (k = 1; k <= 100; k++) {
+            if (k % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (k % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (k % 3 == 0) {
+                System.out.println("Fiz");
+            } else System.out.println(k);
+        }
+
+        System.out.println("BANANA");
+
+//        Scanner responseScanner = new Scanner(System.in);
+//
+//        responseScanner.nextLine(System.in(String, "Hey pardner, I could really go for an integer right now: "));
 
     }
 
