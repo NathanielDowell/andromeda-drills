d96bc151bf3951ececdf519f987f9193b33dabce
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-05-25 11:40:58 -0500

    3 out of 4 exercises complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,10 +5,10 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-        int i = 5;
+        int i = 1;
 
         while (i < 15){
-            System.out.println(i + "!");
+            System.out.println(i + "! And here it is again!! " + i);
             i++;
         }
 
@@ -39,9 +39,20 @@ public class ControlFlowExercises {
 
         System.out.println("BANANA");
 
-//        Scanner responseScanner = new Scanner(System.in);
-//
-//        responseScanner.nextLine(System.in(String, "Hey pardner, I could really go for an integer right now: "));
+        Scanner responseScanner = new Scanner(System.in);
+        System.out.println("Hey pardner, I could really go for an integer right now: ");
+
+        int n = responseScanner.nextInt();
+        System.out.println("You chose " + n + ". Good choice.\n");
+        System.out.println("-----Table of Powers----\n");
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+            for (int f = 1; f<=n; f++) {
+                int squared = f*f;
+                int cubed = f*f*f;
+                //printf allows use of
+                System.out.printf("%-7d|%-9d|%-6d%n", f, squared, cubed);
+            }
 
     }
 
